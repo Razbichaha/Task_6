86e89933f31a25935a4642828b449818fc98d063
Сергей <seregabur@live.ru>
2022-03-23 22:51:25 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalImages = 52;
+            int picturesInARow = 3;
+            int numberOfRows = totalImages / picturesInARow;
+            int imagesStay = totalImages - picturesInARow * numberOfRows;
+
+            Console.WriteLine("Картинок в ряду = " + numberOfRows.ToString());
+            Console.WriteLine("Картинок осталось = " + imagesStay.ToString());
+            Console.ReadLine();
+        }
+    }
+}
